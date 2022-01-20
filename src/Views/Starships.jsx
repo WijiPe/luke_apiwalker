a03b8fe9767c3a4707f8397e52291d21ae7ff6ae
Wiji <peawseaw@gmail.com>
2022-01-19 21:07:55 -0800

    commit

--- src/Views/Starships.jsx
+++ src/Views/Starships.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import axios from "axios";
-import { useHistory} from 'react-router-dom'
 import { useParams } from "react-router-dom";
 import { useEffect } from 'react';
 
